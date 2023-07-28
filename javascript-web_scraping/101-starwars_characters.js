cb2950c36145c65d984bed85c7be12f769a23241
mmasegege <marthamsg2@gmail.com>
2023-07-28 15:08:42 +0200

    allfiles

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
+// Display characters name in the same order of the list  characters in the film response
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
