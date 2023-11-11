bf4d130baff21758e9fad29a2ac799a3cd2d329f
mmasegege <109472984+mmasegege@users.noreply.github.com>
2023-11-11 16:18:06 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const dict = require('./101-data').dict;
-console.log(Object.entries(dict).reduce(function (i, idx) {
-  i[idx[1]] = (i[idx[1]] || []).concat(idx[0]);
-  return i;
-}, {}));
